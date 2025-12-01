8f5abe3e5828600f8fb4dfa41a70dd71f365fc62
SalomeMwende01 <salomemwende01@gmail.com>
2025-12-02 02:14:19 +0300

    added completion on views, created templates, added extra configurations on setting and admin registration

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ def login_view(request):
         # if user has filled in all requires inputs
         if form.is_valid():
             # pick up entries for username and password
-            username = form.clean_data.get('username')
+            username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             # djangomethod authenticate to authenticate and login my user
             user = authenticate(username,password) # queries db looking for the user with mentioned credentials
@@ -48,3 +48,36 @@ def login_view(request):
             form = UserLoginForm() # default http method here is GET
 
         return render(request, 'accounts/login.html', {'form', form})
+    
+
+# logout -> check if our user is logged in - @Login_required
+# if user is logged in then allow this action to run for the user
+@login_required
+def logout_view(request):
+    # use django inbuilt call
+    logout(request)
+    messages.info(request, f"You have logged out!!")
+    return redirect('accounts:login')
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Profile saved successfully")
+            return redirect('accounts:profile')
+        else:
+            form = UserProfileForm(instance=request.user)
+
+        return render(request, 'accounts/profile.html', {'form' : form})
+    class CustomPasswordResetView(PasswordResetView):
+        # interface change
+        template_name = 'accounts/password_react.html'
+        email_template_name = 'accounts/password_reset_email.html'
+        success_url = reverse_lazy('accounts:password_reset_done') # this will launch the confirm view
+
+    class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+        # interface change
+        template_name = 'accounts/password_reset_confirm.html'
+        success_url = reverse_lazy('accounts:password_reset_complete') # this will launch when password is updated
--- mediaHub/settings.py
+++ mediaHub/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'mediaHub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # modification will be here i.e. register global templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,6 +85,9 @@ TEMPLATES = [
         },
     },
 ]
+# URL packaging for static and media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 WSGI_APPLICATION = 'mediaHub.wsgi.application'
 
